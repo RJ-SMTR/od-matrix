554a09c4bc6f869738a6fce2b7e61b374b39e069
ashleyvicary <85631772+ashleyvicary@users.noreply.github.com>
2021-06-17 17:02:23 -0400

    r script to query BigQuery

--- RioProject/OnibusGPS.R
+++ RioProject/OnibusGPS.R
@@ -4,11 +4,15 @@ library(tidyverse)
 library(h3)
 library(leaflet)
 library(colorRamps)
+library(lubridate)
 
 #coords <- Onibus8Days %>% select(latitude,longitude)
 #h3_index <- geo_to_h3(coords, res = 8)
 
-LeafletFilter <- Onibus8Days %>% filter(data == '2021-06-09', linha == 746)
+LeafletFilter <- Onibus8Days %>% filter(data == '2021-06-09'
+                                        ,timestamp_captura < ymd_hms("2021-06-09 14:00:00")
+                                        ,timestamp_captura > ymd_hms("2021-06-09 13:00:00")
+                                        )
 
 h3_index <- geo_to_h3(c(LeafletFilter$latitude,LeafletFilter$longitude), res = 8)
 
@@ -20,7 +24,7 @@ hexagons <- h3_to_geo_boundary_sf(tbl$h3_index) %>%
 
 pal <- colorBin(colorRamps::matlab.like2(10000), domain = hexagons$DataPoints)
 
-leaflet(data = hexagons, width = "100%") %>%
+leaflet(data = hexagons) %>%
   addProviderTiles("Stamen.Toner") %>%
   addPolygons(
     weight = 2,
@@ -29,4 +33,11 @@ leaflet(data = hexagons, width = "100%") %>%
     fillOpacity = 0.8,
     label = ~ sprintf("%i DataPoints (%s)", DataPoints, index)
   )
+
+Summary <- hexagons %>%
+            summarise(mean(DataPoints),
+                      median(DataPoints),
+                      max(DataPoints),
+                      min(DataPoints)
+                      )
 
