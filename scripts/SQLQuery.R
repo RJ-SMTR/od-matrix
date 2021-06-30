b665d9425d301473cf206f5a09e63b82fbbc3c73
ashleyvicary <85631772+ashleyvicary@users.noreply.github.com>
2021-06-30 16:30:20 -0400

    shiny BRT heatmap and maps

--- scripts/SQLQuery.R
+++ scripts/SQLQuery.R
@@ -10,7 +10,12 @@ set_billing_id("rj-smtr") # autenticação para acesso aos dados
 
 data <- read_sql("
 SELECT *
-FROM  `rj-smtr-dev.pytest.BRTCapacity`
+FROM  `rj-smtr-dev.pytest.BRTCapacity` AS BRTCapacity
+  LEFT JOIN `rj-smtr-dev.pytest.BRTStops` AS BRTStops
+    ON BRTCapacity.Line = BRTStops.route_id
+    AND BRTCapacity.TileID = BRTStops.tile_id
+
+WHERE direction_id = 1
                  ")
 
 IntervalData <- data %>% 
@@ -21,13 +26,19 @@ IntervalData <- data %>%
              as.POSIXct(EnterH3Time, format="%H:%M:%S",tz="ET", origin = '1990-01-01'),
              "30 mins")
     ) %>%
-  group_by(Line, Interval) %>%
+  group_by(Line, stop_name, stop_lat, stop_lon, stop_sequence, Interval) %>%
   summarise(   
     TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
     TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
     TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
   as_tibble() 
 
+IntervalDataLine10 <- IntervalData %>%
+  filter(Line == 10) %>%
+  group_by(Line, stop_name, stop_lat, stop_lon, stop_sequence) %>%
+  summarise(AvgTotalCapacity = mean(TotalCapacity))
+
+# Interval data
 p <- ggplot(IntervalData, aes(x = Interval, Line, fill= TotalCapacity)) + 
   geom_tile() +
   scale_x_datetime(date_breaks = "2 hour",
@@ -38,6 +49,97 @@ p <- ggplot(IntervalData, aes(x = Interval, Line, fill= TotalCapacity)) +
 
 ggplotly(p, tooltip=c("TotalCapacity", "TotalSittingCapacity"))
 saveWidget(pp, file=paste0( getwd(), "/figs/ggplotlyHeatmap.html"))
+
+# Just line 10
+p <- ggplot(IntervalDataLine10, aes(x = Interval, y= stop_name, fill= TotalCapacity)) + 
+  geom_tile() +
+  scale_x_datetime(date_breaks = "2 hour",
+                   date_labels = "%H:%M") + 
+  scale_fill_gradient(low="white", high="blue") +
+  theme_ipsum() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplotly(p, tooltip=c("TotalCapacity", "TotalSittingCapacity"))
+
+# Maps
+library(leaflet)
+library(htmltools)
+
+factpal <- colorFactor(topo.colors(5), IntervalDataLine10$stop_sequence)
+
+leaflet(IntervalDataLine10) %>% addTiles() %>%
+  addMarkers(~stop_lon, ~stop_lat
+              ,label = ~paste0(stop_name, ", Total Capacity: ", round(AvgTotalCapacity))
+             #,clusterOptions = ~stop_sequence
+             #,color = ~factpal(stop_sequence)
+             )
+
+# Plotly map
+# change default color scale title
+m <- list(colorbar = list(title = "Total Inches"))
+
+# geo styling
+g <- list(
+  scope = 'brazil',
+  showland = TRUE,
+  landcolor = toRGB("grey83"),
+  subunitcolor = toRGB("white"),
+  countrycolor = toRGB("white"),
+  showlakes = TRUE,
+  lakecolor = toRGB("white"),
+  showsubunits = TRUE,
+  showcountries = TRUE,
+  resolution = 50,
+  projection = list(
+    type = 'conic conformal',
+    rotation = list(lon = -100)
+  ),
+  lonaxis = list(
+    showgrid = TRUE,
+    gridwidth = 0.5,
+    range = c(-140, -55),
+    dtick = 5
+  ),
+  lataxis = list(
+    showgrid = TRUE,
+    gridwidth = 0.5,
+    range = c(20, 60),
+    dtick = 5
+  )
+)
+
+fig <- plot_geo(IntervalDataLine10, lat = ~stop_lat, lon = ~stop_lon, color = ~AvgTotalCapacity)
+fig <- fig %>% add_markers(
+  text = ~paste(IntervalDataLine10$AvgTotalCapacity, "inches"), hoverinfo = "text"
+)
+fig <- fig %>% layout(title = 'US Precipitation 06-30-2015<br>Source: NOAA'
+                      #, geo = g
+                      )
+
+fig
+
+# ridgeline plot
+library(ggridges)
+library(hrbrthemes)
+library(viridis)
+
+data %>%
+  filter(Line == 10) %>%
+  #mutate(text = fct_reorder(text, value)) %>%
+  ggplot( aes(y=stop_name, x=EnterH3Time,  fill=TotalCapacity)) +
+  geom_density_ridges_gradient(bandwidth = 4000) +
+  scale_fill_viridis(discrete=FALSE) +
+  scale_color_viridis(discrete=FALSE) +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  ) +
+  xlab("") +
+  ylab("Assigned Probability (%)")
+
+
 
 write.csv(IntervalData, file = "BRT30minIntervalData.csv")
 write.csv(data, file = "data.csv")
--- shiny/app.R
+++ shiny/app.R
@@ -4,17 +4,26 @@ library(ggmap)
 library(dplyr)
 library(h3)
 library(leaflet)
+library(lubridate)
+library(plotly)
+library(hrbrthemes)
+library(ggplot2)
+library(viridis)
 
 # This rdata file is 300MBs and cannot be synced to GitHub, I'll have to fix this. Hopefully with a SQL Query.
 load("Onibus8Days.rdata")
 load("CapacityData18June2021.rdata")
+IntervalData <- read.csv("IntervalData.csv")
+BRTOperators <- read.csv("BRTOperatorList.csv")
 
 BusList <- distinct(Onibus8Days, ordem)
 DateList <- distinct(Onibus8Days, data)
 LineList <- distinct(Onibus8Days, linha)
+BRTLineList <- distinct(IntervalData, Line)
+BRTOperatorList <- distinct(BRTOperators, Operator)
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Onibus Data Exploration",
+ui <- navbarPage("Data Exploration",
                  tabPanel("GPS Data",
                           # Sidebar with a slider input for number of bins 
                           sidebarLayout(
@@ -73,6 +82,77 @@ ui <- navbarPage("Onibus Data Exploration",
                                   leafletOutput("capacityMap", width = "100%", height = 800)
                               )
                           )
+                 ),
+                 tabPanel("Heatmaps",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  checkboxGroupInput("DayOfWeek", label = h3("Day of Week"), 
+                                                     choices = list("Monday" = 1, 
+                                                                    "Tuesday" = 2, 
+                                                                    "Wednesday" = 3,
+                                                                    "Thursday" = 4,
+                                                                    "Friday" = 5,
+                                                                    "Saturday" = 6,
+                                                                    "Sunday" = 7),
+                                                     selected = c(1, 2, 3, 4, 5, 6, 7)),
+                                  width = 2
+                              ),
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                  plotlyOutput("UtilisationHeatmap"),
+                                  plotlyOutput("CapacityHeatmap")
+
+                              )
+                          )
+                 ),
+                 tabPanel("By Line Data",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  selectInput("ByLineLine", label = h3("Line"), 
+                                              choices = BRTLineList$Line, 
+                                              selected = 1),
+                                  checkboxGroupInput("ByLineDayOfWeek", label = h3("Day of Week"), 
+                                                     choices = list("Monday" = 1, 
+                                                                    "Tuesday" = 2, 
+                                                                    "Wednesday" = 3,
+                                                                    "Thursday" = 4,
+                                                                    "Friday" = 5,
+                                                                    "Saturday" = 6,
+                                                                    "Sunday" = 7),
+                                                     selected = c(1, 2, 3, 4, 5, 6, 7)),
+                                  width = 2
+                              ),
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                  leafletOutput("LineMap"),
+                                  plotlyOutput("LineUtilisationHeatmap"),
+                                  plotlyOutput("LineHeatmap")
+                                 
+                              )
+                          )
+                 ),
+                 tabPanel("Spacial View",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  checkboxGroupInput("SpacialViewOperator", label = h3("Operator"), 
+                                              choices = BRTOperatorList$Operator, 
+                                              selected = "Google"),
+                                  checkboxGroupInput("SpacialViewDayOfWeek", label = h3("Day of Week"), 
+                                                     choices = list("Monday" = 1, 
+                                                                    "Tuesday" = 2, 
+                                                                    "Wednesday" = 3,
+                                                                    "Thursday" = 4,
+                                                                    "Friday" = 5,
+                                                                    "Saturday" = 6,
+                                                                    "Sunday" = 7),
+                                                     selected = c(1, 2, 3, 4, 5, 6, 7)),
+                                  width = 2
+                              ),
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                  plotlyOutput("SpacialView", width = "100%", height = 600)
+                              )
+                          )
                  )
 )
 
@@ -84,6 +164,7 @@ server <- function(input, output) {
                data = Onibus8Days %>% dplyr::filter(ordem == input$BusNumber, data == input$Date), 
                maptype = "toner-lite", color = I("red"))
     })
+    
     output$linePlot <- renderPlot({
         qmplot(longitude, latitude, 
                data = Onibus8Days %>% dplyr::filter(linha == input$Line, data == input$Date), 
@@ -146,7 +227,206 @@ server <- function(input, output) {
                 #label = ~ sprintf("%g DataPoints (%s)", Capacity, index)
             )
     })
+    
+    output$CapacityHeatmap <- renderPlotly({
+        
+        # Interval data clean up input$DayOfWeek
+        IntervalDataFiltered <- IntervalData %>%
+            filter(wday(AsAt) %in% input$DayOfWeek) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Line, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            as_tibble() 
+        
+        p <- ggplot(IntervalDataFiltered, aes(x = Interval, Line, fill= TotalCapacity)) + 
+            geom_tile() +
+            scale_x_datetime(date_breaks = "2 hour",
+                             date_labels = "%H:%M") + 
+            scale_fill_gradient(low="white", high="blue") +
+            theme_ipsum() +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+            ggtitle("Total Capacity")
+        
+        ggplotly(p, tooltip=c("TotalCapacity", "TotalSittingCapacity"))
+    })
+    
+    output$UtilisationHeatmap <- renderPlotly({
+        # Interval data clean up input$DayOfWeek
+        IntervalDataFiltered <- IntervalData %>%
+            filter(wday(AsAt) %in% input$DayOfWeek) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Line, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            mutate(
+                UtilisationRate = log(TotalCapacity) / 10
+            ) %>%
+        as_tibble() 
+        
+        p <- ggplot(IntervalDataFiltered, aes(x = Interval, Line, fill= UtilisationRate)) + 
+            geom_tile() +
+            scale_x_datetime(date_breaks = "2 hour",
+                             date_labels = "%H:%M") + 
+            scale_fill_gradient(low="white", high="red") +
+            theme_ipsum() +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+            ggtitle("Utilisation")
+        
+        ggplotly(p, tooltip=c("UtilisationRate"))
+    })
 
+    output$LineHeatmap <- renderPlotly({
+        # Interval data clean up input$DayOfWeek
+        IntervalDataFiltered <- IntervalData %>%
+            filter(wday(AsAt) %in% input$ByLineDayOfWeek,
+                   Line == input$ByLineLine) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Line, stop_name, stop_lat, stop_lon, stop_sequence, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            as_tibble() 
+        
+        p <- ggplot(IntervalDataFiltered, aes(x = Interval, stop_name, fill= TotalCapacity)) + 
+            geom_tile() +
+            scale_x_datetime(date_breaks = "2 hour",
+                             date_labels = "%H:%M") + 
+            scale_fill_gradient(low="white", high="blue") +
+            theme_ipsum() +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+            ggtitle("Line Capacity")
+        
+        ggplotly(p, tooltip=c("TotalCapacity", "TotalSittingCapacity"))
+    })
+    
+    output$LineUtilisationHeatmap <- renderPlotly({
+        # Interval data clean up input$DayOfWeek
+        IntervalDataFiltered <- IntervalData %>%
+            filter(wday(AsAt) %in% input$ByLineDayOfWeek,
+                   Line == input$ByLineLine) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Line, stop_name, stop_lat, stop_lon, stop_sequence, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            mutate(
+                Utilisation = jitter(log10(TotalCapacity) / 10)
+            ) %>%
+            as_tibble() 
+        
+        p <- ggplot(IntervalDataFiltered, aes(x = Interval, stop_name, fill= Utilisation)) + 
+            geom_tile() +
+            scale_x_datetime(date_breaks = "2 hour",
+                             date_labels = "%H:%M") + 
+            scale_fill_gradient(low="white", high="red") +
+            theme_ipsum() +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+            ggtitle("Utilisation")
+        
+        ggplotly(p, tooltip=c("Utilisation", "TotalSittingCapacity"))
+    })
+    
+    output$LineMap <- renderLeaflet({
+        IntervalDataFiltered <- IntervalData %>%
+            filter(wday(AsAt) %in% input$ByLineDayOfWeek,
+                   Line == input$ByLineLine) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Line, stop_name, stop_lat, stop_lon, stop_sequence, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            as_tibble() 
+        
+        leaflet(IntervalDataFiltered) %>% addTiles() %>%
+            addMarkers(~stop_lon, ~stop_lat
+                       ,label = ~paste0(stop_name, ", Total Capacity: ", round(TotalCapacity))
+                       #,clusterOptions = ~stop_sequence
+                       #,color = ~factpal(stop_sequence)
+            )
+    })
+    
+    output$SpacialView <- renderPlotly({
+        IntervalDataFiltered <- IntervalData %>%
+            left_join(BRTOperators) %>%
+            filter(wday(AsAt) %in% input$SpacialViewDayOfWeek,
+                   Operator == input$SpacialViewOperator) %>%
+            mutate(
+                TotalCapacity = as.numeric(TotalCapacity),
+                SittingCapacity = as.numeric(SittingCapacity),
+                Interval = ceiling_date(
+                    as.POSIXct(EnterH3Time, format="%H:%M:%S"
+                               #,tz="ET"
+                               , origin = '1990-01-01'),
+                    "30 mins")
+            ) %>%
+            group_by(Operator, Line, stop_name, stop_lat, stop_lon, stop_sequence, Interval) %>%
+            summarise(   
+                TotalSittingCapacity = sum(SittingCapacity, na.rm = TRUE),
+                TotalStandingCapacity = sum(StandingCapacity, na.rm = TRUE),
+                TotalCapacity = sum(TotalCapacity, na.rm = TRUE)) %>%
+            as_tibble() 
+        
+        p <- IntervalDataFiltered %>%
+            mutate(text = paste0("Line: ", Line, ", Stop: ", stop_name, ", Total Capacity: ", TotalCapacity)) %>%
+        ggplot( aes(x=stop_lon, y=stop_lat, size = TotalCapacity, color = Operator, text=text)) +
+            geom_point(alpha=1) +
+            scale_size(range = c(0.01, 4), name="Total Capacity") +
+            scale_color_viridis(discrete=TRUE) +
+            theme_ipsum() +
+            theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+            scale_x_continuous(labels = NULL) + 
+            scale_y_continuous(labels = NULL)
+        
+        ggplotly(p, tooltip="text")
+    })
 }
 
 # Run the application 
